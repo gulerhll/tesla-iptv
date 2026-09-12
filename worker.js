566f5df8fc7c2c415fb749ddab222905812d9bd0
gulerhll <gulerhll@gmail.com>
2026-09-12 21:27:01 +0300

    Add files via upload

--- worker.js
+++ worker.js
@@ -24,15 +24,16 @@ export default {
       return new Response("Geçersiz adres", { status: 400, headers: cors });
     }
 
-    if (targetUrl.hostname !== "fabulasp.xyz") {
+    const allowedHosts = new Set(["tinyurl.com", "www.tinyurl.com", "fabulasp.xyz"]);
+    if (!allowedHosts.has(targetUrl.hostname)) {
       return new Response("Bu sunucuya izin verilmiyor", {
         status: 403,
         headers: cors
       });
     }
 
-    async function fetchOrigin(u) {
-      return fetch(u.toString(), {
+    try {
+      const response = await fetch(targetUrl.toString(), {
         method: "GET",
         redirect: "follow",
         headers: {
@@ -40,39 +41,14 @@ export default {
           "Accept": "*/*"
         }
       });
-    }
 
-    function makeResponse(response, usedUrl) {
       const headers = new Headers(response.headers);
       Object.entries(cors).forEach(([k, v]) => headers.set(k, v));
-      headers.set("X-Proxy-Origin", usedUrl.protocol.replace(":", ""));
+
       return new Response(response.body, {
         status: response.status,
         headers
       });
-    }
-
-    try {
-      let usedUrl = new URL(targetUrl.toString());
-
-      try {
-        const response = await fetchOrigin(usedUrl);
-
-        if (usedUrl.protocol === "https:" && response.status >= 500) {
-          const fallbackUrl = new URL(targetUrl.toString());
-          fallbackUrl.protocol = "http:";
-          const fallbackResponse = await fetchOrigin(fallbackUrl);
-          return makeResponse(fallbackResponse, fallbackUrl);
-        }
-
-        return makeResponse(response, usedUrl);
-      } catch (firstError) {
-        if (usedUrl.protocol !== "https:") throw firstError;
-
-        usedUrl.protocol = "http:";
-        const response = await fetchOrigin(usedUrl);
-        return makeResponse(response, usedUrl);
-      }
     } catch (e) {
       return new Response("Sunucu bağlantı hatası: " + (e?.message || String(e)), {
         status: 502,
